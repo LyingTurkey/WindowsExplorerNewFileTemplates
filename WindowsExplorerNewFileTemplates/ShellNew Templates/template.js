fbfca1585095dfc852fb529bf42cd52e9711e9fc
LyingTurkey <lyingturkey@gmail.com>
2026-07-22 01:08:53 -0500

    Add files via upload.

---
+++ WindowsExplorerNewFileTemplates/ShellNew Templates/template.js
@@ -0,0 +1,8 @@
+// Welcome to your new JavaScript file!
+// Run this in Node.js or a browser console.
+
+function main() {
+    console.log("Hello, World!");
+}
+
+main();
---
+++ WindowsExplorerNewFileTemplates/ShellNew Templates/template.py
@@ -0,0 +1,8 @@
+# Welcome to your new Python script!
+# Run this file in your terminal or IDE to see it work.
+
+def main():
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    main()
